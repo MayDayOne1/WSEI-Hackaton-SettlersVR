e367097a51332e549c8c6c0f9da5d046b3bc7a50
pmilowski <pmilowski16@gmail.com>
2023-04-15 01:38:13 +0200

    Collision conditions test

---
+++ Assets/Scripts/PushByHand.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+public class PushByHand : XRGrabInteractable
+{
+    private Rigidbody rb;
+    public float pushForce = 5f;
+    public InputActionReference Fist;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(Fist.action.enabled)
+        {
+            Debug.Log("Action enabled!");
+        }
+        if(collision.gameObject.tag == "Creature")
+        {
+            Rigidbody collidedRigidbody = collision.collider.GetComponent<Rigidbody>();
+            if (collidedRigidbody != null)
+            {
+                Vector3 pushDirection = transform.forward;
+                collidedRigidbody.AddForce(pushDirection * pushForce, ForceMode.VelocityChange);
+            }
+        }
+       
+    }
+}
