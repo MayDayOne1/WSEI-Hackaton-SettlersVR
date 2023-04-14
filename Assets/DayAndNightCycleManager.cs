cf4b6f76b0ffa13f55ee6f8c46e2c934b2f672e2
Szyszzz <79982000+Szyszzz@users.noreply.github.com>
2023-04-14 18:39:50 +0200

    MainLight Reacts to DayAndNightCycleManager

--- Assets/DayAndNightCycleManager.cs
+++ Assets/DayAndNightCycleManager.cs
@@ -2,37 +2,84 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class DayAndNightCycleManager : MonoBehaviour
 {
-    public static DayAndNightCycleManager instance;
+
     public event Action onHourPassed;
 
+    public static DayAndNightCycleManager instance;
+    public Light mainLight;
     public int irlSecondsPerGameHour;
+    public int HoursPassedInADay = 0;
+    public int DaysPassed = 0;
+    public Vector2 DayTime = new Vector2(8, 20);
+    public bool isDay;
+    public float initialSunOffset;
 
-    public bool eventSent = false;
+    private bool wasEventSent = false;
+    private int irlSecPerGameDay;
+    private float dayPercent;
 
-    private int TimeModulo() => (int)Mathf.Round(Time.time) % irlSecondsPerGameHour;
+    private int gameHourModulo() => (int)Mathf.Round(Time.time) % irlSecondsPerGameHour;
+
+    private void OnValidate()
+    {
+        RotateSun();
+        CheckIfDay();
+    }
 
     private void Awake()
     {
         if (instance != null) Destroy(gameObject);
         instance = this;
+        irlSecPerGameDay = 24 * irlSecondsPerGameHour;
     }
 
     private void Update()
     {
-        if(TimeModulo() == 0)
+        SendEventHourPassed();
+        dayPercent = DayPercentage();
+        RotateSun();
+        CheckIfDay();
+    }
+
+    private void SendEventHourPassed()
+    {
+        if (gameHourModulo() == 0)
         {
-            if (!eventSent)
+            if (!wasEventSent)
             {
                 onHourPassed();
-                eventSent = true;
+                HoursPassedInADay++;
+                if (HoursPassedInADay > 23)
+                {
+                    HoursPassedInADay = 0;
+                    DaysPassed++;
+                }
+                wasEventSent = true;
             }
         }
         else
         {
-            eventSent = false;
+            wasEventSent = false;
         }
+    }
+
+    private float DayPercentage() => (Time.time % irlSecPerGameDay) / irlSecPerGameDay;
+
+    private void RotateSun()
+    {
+        Vector3 newRot = new Vector3(initialSunOffset + 360 * dayPercent, 0, 0);
+        mainLight.transform.rotation = Quaternion.Euler(newRot);
+    }
+
+    private void CheckIfDay()
+    {
+        if (HoursPassedInADay > DayTime.x && HoursPassedInADay < DayTime.y)
+            isDay = true;
+        else 
+            isDay = false;
     }
 }
